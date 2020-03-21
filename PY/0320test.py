71d7402d68f746cc7a2c26b93291855fd434b85b
lpc7185657 <lipeng7185657@gmail.com>
2020-03-21 22:06:07 +0800

    加入9-3测试

--- PY/0320test.py
+++ PY/0320test.py
@@ -16,3 +16,25 @@ print(my_rest.cuisine_type.title())
 
 my_rest.describe_restaurant()
 my_rest.open_restaurant()
+
+
+#9-3
+class User():
+    '''创建用户'''
+
+    def __init__(self,first_name,last_name,age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def describe_user(self):
+        print('New User:')
+        print('Name: ' + self.first_name.title() + ' ' + self.last_name.title())
+        print('Age: ' + str(self.age))
+
+    def greet_user(self):
+        print('Hello! ' + self.first_name.title() + ' ' + self.last_name.title() + '.')
+
+a = User('li','pengcheng',29)
+a.describe_user()
+a.greet_user()
