90da0ab97648deed3f76e5bc879e1bef66c78a1e
lpc7185657 <lipeng7185657@gmail.com>
2020-03-22 23:16:37 +0800

    3月22日学习

--- PY/0322test.py
+++ PY/0322test.py
@@ -3,12 +3,23 @@ class Restaurant():
     def __init__(self,restaurant_name,cuisine_type):
         self.asd = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(self.asd.title() + '餐厅')
 
     def open_restaurant(self):
         print(self.asd.title() + '餐厅开业啦！')
+    
+    def a(self,b):
+        self.number_served = b
+        print('我们有 ' + str(b) + ' 人在本餐馆就餐过！')
+    
+    def read(self):
+        print('这家店每天可以就餐的人数为：' + str(self.number_served))
+
+    def increment_number_served(self,c):
+        self.number_served += c
 
 my_rest = Restaurant('aa','bb',)
 print(my_rest.asd.title())
@@ -16,6 +27,11 @@ print(my_rest.cuisine_type.title())
 
 my_rest.describe_restaurant()
 my_rest.open_restaurant()
+
+my_rest.a(25)
+
+my_rest.increment_number_served(200)
+my_rest.read()
 
 
 #9-3
@@ -26,6 +42,7 @@ class User():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        self.login_attempts = 0
     
     def describe_user(self):
         print('New User:')
@@ -36,6 +53,21 @@ class User():
         print('Hello! ' + self.first_name.title() + ' '
          + self.last_name.title() + '.')
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        print(str(self.login_attempts))
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(str(self.login_attempts))
+
+
 a = User('li','pengcheng',29)
 a.describe_user()
 a.greet_user()
+a.increment_login_attempts()
+a.increment_login_attempts()
+a.increment_login_attempts()
+a.increment_login_attempts()
+a.increment_login_attempts()
+a.reset_login_attempts()
